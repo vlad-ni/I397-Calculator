5e27ab446e2ffc39a7835d25cad60b3486c33642
Vladimir Nitsenko <vladimir.nitsenko@gmail.com>
2016-01-08 23:22:59 +0200

    Init

---
+++ app/src/main/java/com/vladimir/calculator/MainActivity.java
@@ -0,0 +1,24 @@
+package com.vladimir.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonNumberClicked (View view)
+    {
+
+    }
+
+    public void buttonOperationClicked (View view)
+    {
+
+    }
+}
